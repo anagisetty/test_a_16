9cb3554bb4dfd42465d61297391599c9d4ea8800
dotnet-code <@dotnet-code>
2023-04-19 13:07:14 +0000

    commit from code202304190714

---
+++ test_a_16/Controllers/SignUpController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Test_A_16.Models;
+
+namespace Test_A_16.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignUpController : ControllerBase
+    {
+        private readonly SignUpContext _context;
+
+        public SignUpController(SignUpContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/SignUp
+        [HttpPost]
+        public async Task<ActionResult<SignUp>> PostUser(SignUp signUp)
+        {
+            // Check if the user already exists
+            if (_context.SignUps.Any(x => x.Email == signUp.Email))
+            {
+                return BadRequest("User already exists");
+            }
+
+            _context.SignUps.Add(signUp);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = signUp.Id }, signUp);
+        }
+    }
+}
